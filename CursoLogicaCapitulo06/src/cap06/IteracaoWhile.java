3a7f94b2dbec0204fe27e5049cef1e97f81540e0
Enzo Lima <enzolima527@gmail.com>
2024-01-15 19:53:25 -0300

    .

--- CursoLogicaCapitulo06/src/cap06/IteracaoWhile.java
+++ CursoLogicaCapitulo06/src/cap06/IteracaoWhile.java
@@ -6,12 +6,27 @@ public class IteracaoWhile {
 //		for(int i = 0; i < 10; i++) {
 //			System.out.println("Um texto qualquer");
 //		}
+		
+//		int i = 0;
+//		while (i < 10) {
+//			if ( i == 5) {
+//				System.out.println("Vai parar");
+//				break;
+//			}
+//			System.out.println("Um texto qualquer.");
+//			i++;
+//		}
+
 		int i = 0;
 		while (i < 10) {
+			if ( i == 5) {
+				System.out.println("Vai continuar");
+				i++;
+				continue;
+			}
 			System.out.println("Um texto qualquer.");
 			i++;
 		}
-
 	}
 
 }
